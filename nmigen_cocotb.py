05253d35ac403f865feaf9291e4b97a20b7ba0e7
akukulanski <akukulanski@gmail.com>
2020-07-14 12:36:46 -0300

    renamed: rom --> extra

--- nmigen_cocotb.py
+++ nmigen_cocotb.py
@@ -61,19 +61,19 @@ def generate_verilog(verilog_file, design, platform, name='top', ports=(), vcd_f
             vcd_file = os.path.abspath(vcd_file)
             f.write(verilog_waveforms.format(vcd_file, name))
 
-def copy_rom_files(rom_sources, path):
-    for f in rom_sources:
+def copy_extra_files(extra_files, path):
+    for f in extra_files:
         shutil.copy(f, path)
 
-def run(design, module, platform=None, ports=(), name='top', verilog_sources=None, rom_sources=None, vcd_file=None):
+def run(design, module, platform=None, ports=(), name='top', verilog_sources=None, extra_files=None, vcd_file=None):
     with tempfile.TemporaryDirectory() as d:
         verilog_file = d + '/nmigen_output.v'
         generate_verilog(verilog_file, design, platform, name, ports, vcd_file)
         sources = [verilog_file]
         if verilog_sources:
             sources.extend(verilog_sources)
-        if rom_sources:
-            copy_rom_files(rom_sources, d)
+        if extra_files:
+            copy_extra_files(extra_files, d)
         os.environ['SIM'] = 'icarus'
         cocotb_run(simulator=Icarus,
                    toplevel=name,
